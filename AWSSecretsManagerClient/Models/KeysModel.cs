ece188ec527333e7f6a9797c89319ea60596a33d
xiangcitoZ <pzxiang1@gmail.com>
2023-06-02 12:57:22 +0200

    Add project files.

---
+++ AWSSecretsManagerClient/Models/KeysModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AWSSecretsManagerClient.Models
+{
+    public class KeysModel
+    {
+        public string MySql { get; set; }
+        public string S3Bucket { get; set; }
+        public string ApiPersonajes { get; set; }
+        public string Champions { get; set; }
+    }
+
+}
---
+++ AWSSecretsManagerClient/Program.cs
@@ -0,0 +1,25 @@
+﻿using AWSSecretsManagerClient.Helpers;
+using AWSSecretsManagerClient.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
+
+Console.WriteLine("Ejemplo Secrets Manager");
+
+string miSecreto = await HelperSecretManager.GetSecretAsync();
+Console.WriteLine(miSecreto);
+//PODEMOS DAR FORMATO A NUESTRO SECRETO
+KeysModel model = JsonConvert.DeserializeObject<KeysModel>(miSecreto);
+Console.WriteLine("Ganador de la Champions: " + model.Champions);
+//SI ESTAMOS EN UNA APP MAS COMPLEJA DONDE NECESITAMOS RECUPERAR MAS 
+//DATOS Y UTILIZARLOS EN DISTINTAS CLASES, AL ESTILO DE IConfiguration
+var provider =
+    new ServiceCollection()
+    .AddSingleton<KeysModel>()
+    .BuildServiceProvider();
+
+//RECUPERAMOS NUESTRO KEYMODEL DE LA INYECCION PARA COMPROBAR 
+//QUE FUNCIONA BIEN
+var service = provider.GetService<KeysModel>();
+Console.WriteLine("Inyectado el Servicio=??? " + service.MySql);
+
+Console.WriteLine("Fin de programa");
